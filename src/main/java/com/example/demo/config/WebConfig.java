ad2243d30a3c34907e6f2c224f743a24d9069f85
xinzhaizisunqi <34813785+xinzhaizisunqi@users.noreply.github.com>
2020-12-13 13:10:38 +0800

    fdgf

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -14,9 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        File file = new File("src/main/resources");
-       System.out.println(file.getAbsolutePath());
-                registry.addResourceHandler("/image/**").addResourceLocations("file:/Users/sunqi/Desktop/work/mylearn/src/main/resources/static/");
+                //registry.addResourceHandler("/image/**").addResourceLocations("file:/Users/sunqi/Desktop/work/mylearn/src/main/resources/static/");
+
+        registry.addResourceHandler("/image/**").addResourceLocations("classpath:/static/");
 
 
 
